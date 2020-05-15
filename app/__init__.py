0910299dbc68f78460dc7c4d2fa230a54c0030aa
Xianfeng <xianyuan@gmail.com>
2020-05-14 22:54:50 -0700

    user api

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
@@ -37,6 +37,9 @@ def create_app(config_class=Config):
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
